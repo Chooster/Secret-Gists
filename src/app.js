96f1629c0f3912b36261d630c16b053261475b67
Chooster <jcooleycoder@gmail.com>
2017-11-10 14:59:59 -0800

    created get / route

--- src/app.js
+++ src/app.js
@@ -1,15 +1,51 @@
 const express = require('express');
-const github = require('github');
+const GithubApi = require('github');
 
 const server = express();
 
+const github = new GithubApi({
+  debug: true
+});
+
+github.authenticate({
+  type: 'oauth',
+  token: process.env.OAUTH_TOKEN
+});
+
+github.gists.create({
+  description: 'the description for this gist',
+  public: false,
+  files: {
+    'TEST.md': {
+      content: '<html><h1>This is a Test!</h1><b>Hello</b></html>'
+    }
+  }
+}).then((response) => {
+  console.log(response);
+})
+.catch((err) => {
+  console.log(err);
+});
+
+server.get('/', (req, res) => {
+  github.gists.getForUser({ username: 'Chooster' })
+  .then((response) => {
+    res.status(200);
+    res.json(response);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+});
+
 server.post('/login', (req, res) => {
-  const { username, oauth_token } = req.body;
+  const { username, oauth } = req.body;
   // TODO log in to GitHub, return success/failure response
 });
 
 server.get('/gists', (req, res) => {
   // TODO retrieve a list of gists for the currently authed user
+
 });
 
 server.listen(3000);
